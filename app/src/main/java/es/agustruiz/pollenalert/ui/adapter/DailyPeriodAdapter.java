46b65ab98aed2055fd8c9d39dc4ce49688563dfa
Agustín Ruiz Linares <correo@agustruiz.es>
2016-02-09 14:26:23 +0100

    [ADD] Different pollen varieties in forecast

--- app/src/main/java/es/agustruiz/pollenalert/ui/adapter/DailyPeriodAdapter.java
+++ app/src/main/java/es/agustruiz/pollenalert/ui/adapter/DailyPeriodAdapter.java
@@ -2,6 +2,8 @@ package es.agustruiz.pollenalert.ui.adapter;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +13,8 @@ import android.widget.TextView;
 
 import java.util.Calendar;
 import java.util.Date;
+
+import javax.net.ssl.ManagerFactoryParameters;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -37,18 +41,14 @@ public class DailyPeriodAdapter extends ArrayAdapter<DailyPeriod> {
     public View getView(int position, View convertView, ViewGroup parent) {
         View row = convertView;
         DailyPeriodHolder holder;
-
         if (row == null) {
             LayoutInflater inflater = ((Activity) context).getLayoutInflater();
             row = inflater.inflate(layoutResourceId, parent, false);
-
             holder = new DailyPeriodHolder(row);
-
             row.setTag(holder);
         } else {
             holder = (DailyPeriodHolder) row.getTag();
         }
-
         DailyPeriod dailyPeriod = data[position];
 
         // Date
@@ -60,40 +60,128 @@ public class DailyPeriodAdapter extends ArrayAdapter<DailyPeriod> {
         holder.dayName.setText(dayOfWeek);
         holder.dayNumber.setText(day);
 
-        // Pollen values and colors
-        PollenDayPeriod pollenDayPeriod = dailyPeriod.getCombined();
-        holder.avgCounter.setText(pollenDayPeriod.getAvgCounter().toString());
-        holder.avgLevel.setText(pollenDayPeriod.getAvgLevel());
-        holder.maxCounter.setText(pollenDayPeriod.getMaxCounter().toString());
-        holder.maxLevel.setText(pollenDayPeriod.getMaxLevel());
-        holder.minCounter.setText(pollenDayPeriod.getMinCounter().toString());
-        holder.minLevel.setText(pollenDayPeriod.getMinLevel());
+        // Preferences
+        SharedPreferences preferences =PreferenceManager.getDefaultSharedPreferences(context);
 
-        // Icon
-        holder.pollenIcon.setColorFilter(getColorFilter(pollenDayPeriod.getAvgLevel()));
+        if(preferences.getBoolean("pref_combined", false)){
+            PollenDayPeriod pollenDayPeriod = dailyPeriod.getCombined();
+            holder.combinedIcon.setColorFilter(getColorFilter(pollenDayPeriod.getAvgLevel()));
+            holder.combinedAvgCounter.setText(pollenDayPeriod.getAvgCounter().toString());
+            holder.combinedAvgLevel.setText(pollenDayPeriod.getAvgLevel());
+            holder.combinedMaxCounter.setText(pollenDayPeriod.getMaxCounter().toString());
+            holder.combinedMaxLevel.setText(pollenDayPeriod.getMaxLevel());
+            holder.combinedMinCounter.setText(pollenDayPeriod.getMinCounter().toString());
+            holder.combinedMinLevel.setText(pollenDayPeriod.getMinLevel());
+        }else{
+            holder.combinedView.setVisibility(View.GONE);
+        }
+
+        if(preferences.getBoolean("pref_olive", false)){
+            PollenDayPeriod pollenDayPeriod = dailyPeriod.getOlive();
+            holder.oliveIcon.setColorFilter(getColorFilter(pollenDayPeriod.getAvgLevel()));
+            holder.oliveAvgCounter.setText(pollenDayPeriod.getAvgCounter().toString());
+            holder.oliveAvgLevel.setText(pollenDayPeriod.getAvgLevel());
+            holder.oliveMaxCounter.setText(pollenDayPeriod.getMaxCounter().toString());
+            holder.oliveMaxLevel.setText(pollenDayPeriod.getMaxLevel());
+            holder.oliveMinCounter.setText(pollenDayPeriod.getMinCounter().toString());
+            holder.oliveMinLevel.setText(pollenDayPeriod.getMinLevel());
+        }else{
+            holder.oliveView.setVisibility(View.GONE);
+        }
+
+        if(preferences.getBoolean("pref_grass", false)){
+            PollenDayPeriod pollenDayPeriod = dailyPeriod.getGrass();
+            holder.grassIcon.setColorFilter(getColorFilter(pollenDayPeriod.getAvgLevel()));
+            holder.grassAvgCounter.setText(pollenDayPeriod.getAvgCounter().toString());
+            holder.grassAvgLevel.setText(pollenDayPeriod.getAvgLevel());
+            holder.grassMaxCounter.setText(pollenDayPeriod.getMaxCounter().toString());
+            holder.grassMaxLevel.setText(pollenDayPeriod.getMaxLevel());
+            holder.grassMinCounter.setText(pollenDayPeriod.getMinCounter().toString());
+            holder.grassMinLevel.setText(pollenDayPeriod.getMinLevel());
+        }else{
+            holder.grassView.setVisibility(View.GONE);
+        }
+
+        if(preferences.getBoolean("pref_birch", false)){
+            PollenDayPeriod pollenDayPeriod = dailyPeriod.getBirch();
+            holder.birchIcon.setColorFilter(getColorFilter(pollenDayPeriod.getAvgLevel()));
+            holder.birchAvgCounter.setText(pollenDayPeriod.getAvgCounter().toString());
+            holder.birchAvgLevel.setText(pollenDayPeriod.getAvgLevel());
+            holder.birchMaxCounter.setText(pollenDayPeriod.getMaxCounter().toString());
+            holder.birchMaxLevel.setText(pollenDayPeriod.getMaxLevel());
+            holder.birchMinCounter.setText(pollenDayPeriod.getMinCounter().toString());
+            holder.birchMinLevel.setText(pollenDayPeriod.getMinLevel());
+        }else{
+            holder.birchView.setVisibility(View.GONE);
+        }
+
+        if(preferences.getBoolean("pref_ragweed", false)){
+            PollenDayPeriod pollenDayPeriod = dailyPeriod.getRagweed();
+            holder.ragweedIcon.setColorFilter(getColorFilter(pollenDayPeriod.getAvgLevel()));
+            holder.ragweedAvgCounter.setText(pollenDayPeriod.getAvgCounter().toString());
+            holder.ragweedAvgLevel.setText(pollenDayPeriod.getAvgLevel());
+            holder.ragweedMaxCounter.setText(pollenDayPeriod.getMaxCounter().toString());
+            holder.ragweedMaxLevel.setText(pollenDayPeriod.getMaxLevel());
+            holder.ragweedMinCounter.setText(pollenDayPeriod.getMinCounter().toString());
+            holder.ragweedMinLevel.setText(pollenDayPeriod.getMinLevel());
+        }else{
+            holder.ragweedView.setVisibility(View.GONE);
+        }
+
+
+
 
         return row;
     }
 
     static class DailyPeriodHolder {
-        @Bind(R.id.dayName)
-        TextView dayName;
-        @Bind(R.id.dayNumber)
-        TextView dayNumber;
-        @Bind(R.id.pollenIcon)
-        ImageView pollenIcon;
-        @Bind(R.id.avgCounter)
-        TextView avgCounter;
-        @Bind(R.id.avgLevel)
-        TextView avgLevel;
-        @Bind(R.id.maxCounter)
-        TextView maxCounter;
-        @Bind(R.id.maxLevel)
-        TextView maxLevel;
-        @Bind(R.id.minCounter)
-        TextView minCounter;
-        @Bind(R.id.minLevel)
-        TextView minLevel;
+        @Bind(R.id.dayName) TextView dayName;
+        @Bind(R.id.dayNumber) TextView dayNumber;
+
+        @Bind(R.id.combinedView) View combinedView;
+        @Bind(R.id.combinedIcon) ImageView combinedIcon;
+        @Bind(R.id.combinedAvgCounter) TextView combinedAvgCounter;
+        @Bind(R.id.combinedAvgLevel) TextView combinedAvgLevel;
+        @Bind(R.id.combinedMaxCounter) TextView combinedMaxCounter;
+        @Bind(R.id.combinedMaxLevel) TextView combinedMaxLevel;
+        @Bind(R.id.combinedMinCounter) TextView combinedMinCounter;
+        @Bind(R.id.combinedMinLevel) TextView combinedMinLevel;
+
+        @Bind(R.id.oliveView) View oliveView;
+        @Bind(R.id.oliveIcon) ImageView oliveIcon;
+        @Bind(R.id.oliveAvgCounter) TextView oliveAvgCounter;
+        @Bind(R.id.oliveAvgLevel) TextView oliveAvgLevel;
+        @Bind(R.id.oliveMaxCounter) TextView oliveMaxCounter;
+        @Bind(R.id.oliveMaxLevel) TextView oliveMaxLevel;
+        @Bind(R.id.oliveMinCounter) TextView oliveMinCounter;
+        @Bind(R.id.oliveMinLevel) TextView oliveMinLevel;
+
+        @Bind(R.id.grassView) View grassView;
+        @Bind(R.id.grassIcon) ImageView grassIcon;
+        @Bind(R.id.grassAvgCounter) TextView grassAvgCounter;
+        @Bind(R.id.grassAvgLevel) TextView grassAvgLevel;
+        @Bind(R.id.grassMaxCounter) TextView grassMaxCounter;
+        @Bind(R.id.grassMaxLevel) TextView grassMaxLevel;
+        @Bind(R.id.grassMinCounter) TextView grassMinCounter;
+        @Bind(R.id.grassMinLevel) TextView grassMinLevel;
+
+        @Bind(R.id.birchView) View birchView;
+        @Bind(R.id.birchIcon) ImageView birchIcon;
+        @Bind(R.id.birchAvgCounter) TextView birchAvgCounter;
+        @Bind(R.id.birchAvgLevel) TextView birchAvgLevel;
+        @Bind(R.id.birchMaxCounter) TextView birchMaxCounter;
+        @Bind(R.id.birchMaxLevel) TextView birchMaxLevel;
+        @Bind(R.id.birchMinCounter) TextView birchMinCounter;
+        @Bind(R.id.birchMinLevel) TextView birchMinLevel;
+
+        @Bind(R.id.ragweedView) View ragweedView;
+        @Bind(R.id.ragweedIcon) ImageView ragweedIcon;
+        @Bind(R.id.ragweedAvgCounter) TextView ragweedAvgCounter;
+        @Bind(R.id.ragweedAvgLevel) TextView ragweedAvgLevel;
+        @Bind(R.id.ragweedMaxCounter) TextView ragweedMaxCounter;
+        @Bind(R.id.ragweedMaxLevel) TextView ragweedMaxLevel;
+        @Bind(R.id.ragweedMinCounter) TextView ragweedMinCounter;
+        @Bind(R.id.ragweedMinLevel) TextView ragweedMinLevel;
 
         public DailyPeriodHolder(View view) {
             ButterKnife.bind(this, view);
